ede8dfe44602fea5d166c6038c6377e73310085f
nisharmultani <nisharmultani7@gmail.com>
2022-05-02 14:08:42 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <!-- brand -->
+<section>
+  <div class="h-96 bg-white">
+    <div>
+  <h2 class="text-5xl font-bold font-sans text-black text-center pt-40 mx-96 pb-20 ">The trusted market leader in talent transformation.</h2>
+  </div>
+  <div class="mx-96 text-center">
+  <img src="../assets/image/brande.webp" alt=" brande image">
+  </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+  </template>
+
+<script>
+export default {
+}
+</script>
